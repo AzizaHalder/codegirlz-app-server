76cf627d77ca415b02d14bb8e0087c2ea824fa09
Nicole <92593875+ozzleme@users.noreply.github.com>
2023-03-19 15:54:34 +0530

    feat(user.model): create user model

--- models/User.model.js
+++ models/User.model.js
@@ -1,23 +1,17 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    email: {
-      type: String,
-      required: [true, "Email is required."],
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    password: {
-      type: String,
-      required: [true, "Password is required."],
-    },
-    name: {
-      type: String,
-      required: [true, "Name is required."],
-    },
+    email: { type: String, required: [true, "Email is required."], unique: true, lowercase: true, trim: true, match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"] },
+    password: { type: String, required: [true, "Password is required."], },
+    name: { type: String, required: [true, "Name is required."], },
+    currentLocation: { type: String, required: [true, "Current Location is required."], },
+    city: { type: String, },
+    level: { type: String, enum: ["Entry Level", "Junior", "Intermediate", "Senior", "Lead"], required: true, },
+    // is this a valid match for URL using regex? 
+    linkedin: { type: String, match: [/(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/, "Please use valid URL"] },
+    github: { type: String, match: [/(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/, "Please use valid URL"] },
+    newOpps: { type: Boolean, },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
