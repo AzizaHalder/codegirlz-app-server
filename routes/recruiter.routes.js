7fe6daec01365a33cad5a3796b803b51c6aecf51
AzizaHalder <aziza.halder@sap.com>
2023-04-10 21:18:15 +0200

    feat(recruiter.routes): add route to recruiter to GET all user open to jobs and a POST route to contact user about jobs

--- routes/recruiter.routes.js
+++ routes/recruiter.routes.js
@@ -3,6 +3,46 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 const Recruiter = require("../models/Recruiter.model");
+const User = require("../models/User.model");
+
+// GET users who are open to new opportunities
+router.get("/job-candidates", (req, res, next) => {
+  User.find({ newOpp: true })
+    .then((candidates) => res.status(200).json(candidates))
+    .catch((err) => res.json(err));
+});
+
+// PUT /recruiter/edit/:recruiterId --> edit profile profile
+router.put("/edit/:recruiterId", (req, res, next) => {
+  const { recruiterId } = req.params;
+
+  Recruiter.findByIdAndUpdate(recruiterId, req.body, { new: true })
+    .then((updatedProfile) => res.json(updatedProfile))
+    .catch((err) => res.json(err));
+});
+
+// DELETE /recruiter/edit/:recruiterId --> delete profile profile
+router.delete("/edit/:recruiterId", (req, res, next) => {
+  const { recruiterId } = req.params;
+
+  Recruiter.findByIdAndDelete(recruiterId)
+    .then((deleteProfile) => res.json(deleteProfile))
+    .catch((error) => res.json(error));
+});
+
+// POST to contact job candidate
+router.post("/:recruiterId/job-opportunities", (req, res, next) => {
+  const { recruiterId } = req.params;
+  const { userId } = req.body;
+
+  Recruiter.findByIdAndUpdate(
+    recruiterId,
+    { $push: { jobCandidates: userId } },
+    { new: true }
+  )
+    .then((candidates) => res.json(candidates))
+    .catch((err) => res.json(err));
+});
 
 // GET /profile/:recruiterId --> show user profile
 router.get("/:recruiterId", (req, res, next) => {
@@ -19,24 +59,6 @@ router.get("/:recruiterId", (req, res, next) => {
   Recruiter.findById(recruiterId)
     .select("-password -email")
     .then((recruiter) => res.status(200).json(recruiter))
-    .catch((error) => res.json(error));
-});
-
-// PUT /recruiter/edit/:recruiterId --> edit profile profile
-router.put("/edit/:recruiterId", (req, res, next) => {
-  const { recruiterId } = req.params;
-
-  Recruiter.findByIdAndUpdate(recruiterId, req.body, { new: true })
-    .then((updatedProfile) => res.json(updatedProfile))
-    .catch((error) => res.json(error));
-});
-
-// DELETE /recruiter/edit/:recruiterId --> delete profile profile
-router.delete("/edit/:recruiterId", (req, res, next) => {
-  const { recruiterId } = req.params;
-
-  Recruiter.findByIdAndDelete(recruiterId)
-    .then((deleteProfile) => res.json(deleteProfile))
     .catch((error) => res.json(error));
 });
 
